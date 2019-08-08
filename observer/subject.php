140a5a980099c46918c86f11e32f09af8e0695c2
Lucas <llanga@student.wethinkcode.co.za>
2019-08-08 08:56:14 +0200

    Add files via upload

---
+++ observer/subject.php
@@ -0,0 +1,23 @@
+<?
+	class ConcreteSubject implements SplSubject {
+		private 	$observers;
+		private 	$data
+
+		public function setObservers() {
+			$this->observers = new SplObjectStorage();
+		}
+
+		public function attach(SplObserver $observer ) {
+			$this->observers->attach($observer);
+		}
+
+		public function detach(SplObserver $observer) {
+			$this->observers->detach($observer);
+		}
+
+		public function notify() {
+			foreach ( $this->observers as $observer) {
+				$observer->update($this);
+			}
+		}
+	}
